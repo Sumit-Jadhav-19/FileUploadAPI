10ef09b18a623b55c549ddca391d411ac95792d8
Sumit Jadhav <jadhavsumit052@gmail.com>
2025-04-14 15:53:09 +0530

    Innitial commit : File upload API

---
+++ FileUploadAPI/Controllers/UploadController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(IWebHostEnvironment env, ILogger<UploadController> logger)
+        {
+            _env = env;
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("file")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file found");
+            }
+
+            var uploadPath = Path.Combine(_env.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            var allowedTypes = new[] { "image/jpeg", "image/png", "image/jpg", "image/gif" };
+            if (!allowedTypes.Contains(file.ContentType))
+                return BadRequest("Invalid file type");
+
+            var filePath = Path.Combine(uploadPath, "test.png");
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            _logger.LogInformation($"Request Scheme: {Request.Scheme}");
+            _logger.LogInformation($"Request Host: {Request.Host}");
+
+            var fileUrl = $"{Request.Scheme}://{Request.Host}/uploads/{file.FileName}";
+            return Ok(new { filePath = fileUrl });
+        }
+    }
+}
